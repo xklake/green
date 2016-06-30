4532c0454f54051b426168a394929f838b51c305
Qiang Li <joneleesz@hotmail.com>
2016-06-30 13:12:42 +0100

    add error page

---
+++ views/default/error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiang
+ * Date: 6/30/16
+ * Time: 1:10 PM
+ */
+?>
+
+<section id="error" class="container text-center">
+    <h1>Error</h1>
+    <p>The page does not exist, please go back to home page or contact website owner.</p>
+    <a class="btn btn-primary" href="<?=Yii::$app->homeUrl?>">Go back home</a>
+</section><!--/#error-->
+
